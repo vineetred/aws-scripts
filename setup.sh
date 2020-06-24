cd8c34d3abd950c1aa4a3e98484f8c9f46b6c904
Vineet Reddy <vineetreddy@live.com>
2020-06-24 17:44:27 +0530

    Sanitized Password Input

--- setup.sh
+++ setup.sh
@@ -11,20 +11,30 @@ echo "
 echo
 
 if [ "$1" == "-h" ]; then
-  echo "Usage: `basename $0` <ACCESS-KEY> <SECRET-ACCESS-KEY> <REGION-CODE> <ECS-PROFILE-NAME>"
+  echo "Usage: `basename $0` <REGION-CODE> <ECS-PROFILE-NAME>"
   echo "Accepted region format - eu-west-2"
   echo "Refer to https://docs.aws.amazon.com/general/latest/gr/rande.html for correct codes"
+  echo "Prompt will ask you for ACCESS KEY credentials."
   echo
   exit 1
 fi
 
-if [ $# -le 2 ]
+if [ $# -le 1 ]
   then
     echo "No arguments supplied/Missing args"
     exit 1
 fi
 
-
+echo "You will not see your keys"
+echo
+echo -n "ACCESS_KEY_ID:"
+read -s ACCESS_KEY_ID
+echo
+echo -n "SECRET_ACCESS_KEY:"
+read -s SECRET_ACCESS_KEY 
+echo
+# Deleting the keys from the history
+history -c
 
 echo "Updating libraries"
 sudo apt-get update
@@ -51,9 +61,9 @@ ecs-cli -v && echo -e "${ORANGE}ECS CLI OK"
 echo -e "${ORANGE} Setting up the AWS Profile"
 
 
-aws configure set aws_access_key_id $1
-aws configure set aws_secret_access_key $2
-aws configure set default.region $3
+aws configure set aws_access_key_id $ACCESS_KEY_ID
+aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
+aws configure set default.region $1
 
 
 echo -e "${ORANGE} Setting up the IAM Role"
@@ -63,13 +73,13 @@ wget https://bucket-409rez.s3.ap-south-1.amazonaws.com/uploads/task-execution-as
 
 # UNCOMMENT IN CASE TASK DOES NOT EXIST ALREADY
 # CHANGE THE ROLE NAME IN ECS-PARAMS.YML IF CHANGED HERE!
-# aws iam --region $3 create-role --role-name ecsTaskExecutionRole --assume-role-policy-document file://task-execution-assume-role.json
+aws iam --region $1 create-role --role-name ecsTaskExecutionRole --assume-role-policy-document file://task-execution-assume-role.json
 
-aws iam --region $3 attach-role-policy --role-name ecsTaskExecutionRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy
+aws iam --region $1 attach-role-policy --role-name ecsTaskExecutionRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy
 rm task-execution-assume-role.json
 
 echo -e "${ORANGE} Setting up ECS CLI profile"
 
-ecs-cli configure profile --access-key $1 --secret-key $2 --profile-name $4
+ecs-cli configure profile --access-key $ACCESS_KEY_ID --secret-key $SECRET_ACCESS_KEY --profile-name $2
 
 echo -e "${ORANGE} Created a IAM role that can now spawn ECS instances. Run ./cluster_start.sh for deployment"
